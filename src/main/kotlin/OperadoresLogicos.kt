def6992298c94b272cc7b7c0076e0a77278abcd6
Joao Vitor <joaovitorcfdev@gmail.com>
2022-07-25 22:04:24 -0300

    variaveis e operadores logicos

---
+++ src/main/kotlin/OperadoresLogicos.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val idade: Int = 25
+    val check: Int = 15
+
+    if (idade >= check) {
+       println("É adulto")
+    } else {
+        println("Não é adulto")
+    }
+}
---
+++ src/main/kotlin/Variaveis.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val name: String = "João Vitor"
+    val fullname = name + " Costa Ferreira"
+
+    val peso: Double = 80.0
+    val idade: Int = 25
+
+    println(fullname)
+}
